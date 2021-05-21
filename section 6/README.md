2679a987125e79b23b28df4d41892ca17d0d44cb
AhmadJaber <jaber131706@gmail.com>
2021-05-21 11:13:23 +0600

    :zap: filtering data & operators

--- section 6/README.md
+++ section 6/README.md
@@ -37,17 +37,38 @@ This different types of Operators can be combined to make more complex queries
 
 **Topics Covered**
 
-- Execution order with AND, OR Operators
+- Comparison, Logical and Arithmetic operators ✅
+- AND operator ✅
+- OR operator ✅
+- Combining AND and OR Operator ✅
+- What goes before & after WHERE clause ✅
+- Execution order with AND, OR Operators ✅
 
   - AND processed first and the OR is processed second
   - SQL processes AND Operator like multiplication & OR operator like addition unless i include parenthesis
   - without parenthesis, `3*2+1 = 7`, with parenthesis, `3*(2+1) = 9`.
   - it is always better to use parentheses whenever i am using multiple operators.
 
-- Can u use column aliase with WHERE?
+- Can u use column aliase with WHERE? ✅
 
   - no, if i use column aliases in SELECT clause, i can not use them in WHERE clause.
   - but for a sub-query i can use that.
 
-- Order of Execution of WHERE clause in SELECT
+- Order of Execution of WHERE clause in SELECT ✅
+
   - FROM > WHERE > SELECT > ORDER BY
+
+- Using Logical operators ✅
+- Using LIMIT & OFFSET ✅
+- Using FETCH ✅
+
+  > code is in `./queries/movies.sql`
+
+- Using IN, NOT IN ✅
+- Using BETWEEN, NOT BETWEEN ✅
+- Using LIKE, ILIKE ✅
+- Using IS NULL, IS NOT NULL ✅
+- Concatenation techniques ✅
+- Concatenation with ||, CONCAT, CONCAT_WS ✅
+
+  > code is in `./queries/movies_two.sql`
--- section 6/queries/movies_two.sql
+++ section 6/queries/movies_two.sql
@@ -7,7 +7,7 @@
 -- syntax: value NOT IN (value1, value2, .....), where value can be he column/field
 
 -- IN will return true if value matches any value in the list
--- NOR IN will return true if value does not match any value in the list
+-- NOT IN will return true if value does not match any value in the list
 
 -- get all movies for english, chinese, japanese languages
 -- first the alternative way using OR operator
@@ -82,3 +82,164 @@ WHERE
 	AND (movie_length >= 100 AND movie_length <= 200)
 ORDER BY
 	movie_length;
+
+-- -------------------------------------------------
+-- LIKE & ILIKE operators
+
+-- Operators to query data using pattern matching
+-- LIKE is case-sensitive & ILIKE is not case-sensitive
+-- Both let u search for patterns in strings by using two special characters
+-- percent sign (%)
+-- underscore sign (_)
+
+-- syntax: value LIKE pattern
+-- syntax: value ILIKE pattern
+
+-- get all acotors first_names where first_name starting with 'A'
+SELECT
+*
+FROM actors
+WHERE
+	first_name LIKE 'A%'
+ORDER BY
+	first_name;
+
+-- get all the actors name whrere first_name with 5 characters only
+SELECT
+	first_name
+FROM actors
+WHERE
+	first_name LIKE '_____'
+ORDER BY
+	first_name;
+
+-- get all the actors name whrere first_name contains 'l' on the second place
+SELECT
+*
+FROM actors
+WHERE
+	first_name LIKE '_l%'
+ORDER BY
+	first_name;
+
+-- get record from actors where actor name is 'tim'
+SELECT
+*
+FROM actors
+WHERE
+	first_name ILIKE '%tim%'
+ORDER BY
+	first_name;
+
+-- As LIKE & ILIKE searches for patterns, performance on large databases can be slow
+-- we can improve performances using indexes or more
+
+-- ------------------------------------------------------
+-- Using IS NULL & IS NOT NULL keywords
+
+-- IS NULL operator to check if a value is null
+-- IS NOT NULL operator to check if a value is not null
+
+-- IS NULL in a WHERE clause returns only values that are NULL
+-- IS NOT NULL in a WHERE clause returns only values that are not NULL
+
+-- syntax: value IS NULL,
+-- syntax: value IS NOT NULL
+
+-- FIND list of actors with missing birthdate
+SELECT
+*
+FROM actors
+WHERE
+	date_of_birth IS NULL
+ORDER BY
+	date_of_birth;
+
+-- find list of actors with missing birth date or missing first_name
+SELECT
+*
+FROM actors
+WHERE
+	date_of_birth IS NULL
+	OR first_name IS NULL
+ORDER BY
+	date_of_birth;
+
+-- GET list of movies where domestic_revenues are not null
+SELECT
+*
+FROM movies_revenues
+WHERE
+	revenues_domestic IS NOT NULL
+ORDER BY
+	revenues_domestic;
+
+-- for null checking, IS NULL or IS NOT NULL is the best way for all types
+
+-- ------------------------------------------------------
+-- Concatenation techniques
+
+-- concatenation operator ||
+-- SELECT 'string' || 'string' AS new_string;
+
+-- combining columns together
+-- SELECT CONCAT(col_one, col_two) AS new_string;
+
+-- combining columns together CONCAT_WS, which gives us a to set seperator
+
+-- lets combine string 'hello' and 'world'
+SELECT 'hello' || ' ' || 'world';
+
+-- lets combine actors first & last name as 'Actor Name'
+SELECT
+	first_name || ' ' || last_name AS "Actor Name"
+FROM actors
+ORDER BY
+	"Actor Name";
+
+-- alternative
+SELECT
+	CONCAT(first_name, ' ', last_name) AS "Full Name"
+FROM actors
+ORDER BY
+	"Full Name";
+
+-- alternative using CONCAT_WS
+SELECT
+	CONCAT_WS(' ', first_name, last_name) AS "Full Names"
+FROM actors
+ORDER BY
+	"Full Names";
+
+-- lets print first_name, last_name, date_of_birth of all actors separated by comma
+SELECT
+	CONCAT_WS(', ', first_name, last_name, date_of_birth) AS "Actor Info"
+FROM actors
+ORDER BY
+	date_of_birth;
+
+-- ---------------------------------------------
+-- when concatenation how NULL values are handled
+
+-- Are they ignore null values?
+-- using ||
+SELECT 'HELLO' || NULL || 'world';
+-- gives us [null]
+
+-- using CONCAT
+SELECT
+	revenues_domestic,
+	revenues_international,
+	CONCAT(revenues_domestic, ' | ', revenues_international) AS "Total Revenues"
+FROM movies_revenues;
+
+-- the problem of using CONCAT with NULL value is if put any separator like '|'
+-- that will be remained, the row will not be empty.
+-- like, CONCAT([null], ' | ', [null]), the | will be remaining, which is not good
+
+-- for that we can use CONCAT_WS, which is more smart
+SELECT
+	revenues_domestic,
+	revenues_international,
+	CONCAT_WS(' | ', revenues_domestic, revenues_international) AS "Total Revenues"
+FROM movies_revenues;
